bac29250246b1a578ee0c1f3559414f1e151f637
Petteri <petteri.malkavaara1@gmail.com>
2021-01-22 17:14:28 +0200

    Initial commit

---
+++ src/main/java/me.petterim1.worldspawns/Main.java
@@ -0,0 +1,28 @@
+package me.petterim1.worldspawns;
+
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerRespawnEvent;
+import cn.nukkit.plugin.PluginBase;
+
+import java.util.List;
+
+public class Main extends PluginBase implements Listener {
+
+    private List<String> excludedWorlds;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        excludedWorlds = getConfig().getStringList("excludedWorlds");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e) {
+        System.out.println(e.getPlayer().getLevel().getName());
+        if (!excludedWorlds.contains(e.getPlayer().getLevel().getName())) {
+            e.setRespawnPosition(e.getPlayer().getLevel().getSafeSpawn());
+        }
+    }
+}
